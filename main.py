6e753e8b833340683712de084d729e3c6718d560
Hiroaki Fujiwara <hiroaki.tokyu8500@gmail.com>
2024-08-21 19:23:03 +0900

    作った

--- main.py
+++ main.py
@@ -1,90 +1,80 @@
-import os
-import sys
-import logging
+import tecplot as tp
+from tecplot.exception import *
+from tecplot.constant import *
+import numpy as np 
+import matplotlib.pyplot as plt
+# Uncomment the following line to connect to a running instance of Tecplot 360:
+# tp.session.connect()
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-import tecplot
+# 指定された範囲と分割数
+start = 0.15  # 範囲の開始点
+end = 1.0    # 範囲の終了点
+n = 30       # 分割数
 
-# Run this script with "-c" to connect to Tecplot 360 on port 7600
-# To enable connections in Tecplot 360, click on:
-#   "Scripting" -> "PyTecplot Connections..." -> "Accept connections"
-if '-c' in sys.argv:
-    tecplot.session.connect()
+# 各区間の中心点を計算
+r_R = np.linspace(start, end, n+1)  # 分割点を計算
+r_R_centers = (r_R[:-1] + r_R[1:]) / 2  # 中心点を計算
+span_array = r_R_centers * (1/0.202)
 
-examples_dir = tecplot.session.tecplot_examples_directory()
-datafile = os.path.join(examples_dir, 'OneraM6wing', 'OneraM6_SU2_RANS.plt')
-dataset = tecplot.data.load_tecplot(datafile)
+cl_array = []
+# ループを回す外枠
+for span in span_array:
+  print ("CURRENT SPAN : " , span * 0.202)
+  tp.macro.execute_command("""$!ReadDataSet  '\"C:\\Users\\{user}\\Documents\\m1\\07_spanwise-cl-distribution\\surfave.plt\" '
+    ReadDataOption = New
+    ResetStyle = Yes
+    VarLoadMode = ByName
+    AssignStrandIDs = Yes
+    VarNameList = '\"X\" \"Y\" \"Z\" \"cp_ave\" \"cp_rms\" \"cfx_ave\" \"cfy_ave\" \"cfz_ave\"'""")
+  tp.active_frame().plot_type=PlotType.Cartesian3D
+  tp.active_frame().plot().rgb_coloring.red_variable_index=3
+  tp.active_frame().plot().rgb_coloring.green_variable_index=3
+  tp.active_frame().plot().rgb_coloring.blue_variable_index=3
+  tp.active_frame().plot().slice(0).edge.show=True
+  tp.active_frame().plot().slice(0).slice_source=SliceSource.SurfaceZones
+  tp.active_frame().plot().contour(0).variable_index=3
+  tp.active_frame().plot().contour(1).variable_index=4
+  tp.active_frame().plot().contour(2).variable_index=5
+  tp.active_frame().plot().contour(3).variable_index=6
+  tp.active_frame().plot().contour(4).variable_index=7
+  tp.active_frame().plot().contour(5).variable_index=3
+  tp.active_frame().plot().contour(6).variable_index=3
+  tp.active_frame().plot().contour(7).variable_index=3
+  tp.active_frame().plot(PlotType.Cartesian3D).show_slices=True
+  tp.active_frame().plot().slice(0).origin.x=span
+  tp.active_frame().plot().slices(0).extract(transient_mode=TransientOperationMode.AllSolutionTimes)
+  tp.data.save_tecplot_ascii(f'C:\\Users\\{user}\\Documents\\m1\\07_spanwise-cl-distribution\\slice-x-is-{span}.dat',
+      zones=[1],
+      variables=[1,3],
+      include_text=False,
+      precision=9,
+      include_geom=False,
+      use_point_format=True)
+  # End Macro.
+  print("END TECPLOT MACRO")
+  slice_data = np.loadtxt(f'slice-x-is-{span}.dat', skiprows=8)
+  x = slice_data[:,0]
+  x = x / (np.max(x)-np.min(x)) 
+  x = x - np.min(x)
+  y = slice_data[:,1] * (1.0/(0.03514*span))**2
 
-frame = tecplot.active_frame()
-frame.plot_type = tecplot.constant.PlotType.Cartesian3D
-frame.plot().show_contour = True
+  plt.clf()
+  plt.plot(x, y)
+  # plt.show()
 
-# ensure consistent output between interactive (connected) and batch
-frame.plot().contour(0).levels.reset_to_nice()
+  area = 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+  print(f"閉曲線の面積: {area}")
+  cl_array.append(area)
+  print("END AREA CALCULATION")
 
-# export image of wing
-tecplot.export.save_png('wing.png', 600, supersample=3)
+# plot cl - r/R
+plt.clf()
+plt.plot(r_R_centers, cl_array)
+plt.xlabel('r/R')
+plt.ylabel('Cl')
+plt.ylim(0, 1.5)
+plt.show()
 
-# extract an arbitrary slice from the surface data on the wing
-extracted_slice = tecplot.data.extract.extract_slice(
-    origin=(0, 0.25, 0),
-    normal=(0, 1, 0),
-    source=tecplot.constant.SliceSource.SurfaceZones,
-    dataset=dataset)
-
-extracted_slice.name = 'Quarter-chord C_p'
-
-# get x from slice
-extracted_x = extracted_slice.values('x')
-
-# copy of data as a numpy array
-x = extracted_x.as_numpy_array()
-
-# normalize x
-xc = (x - x.min()) / (x.max() - x.min())
-extracted_x[:] = xc
-
-# switch plot type in current frame
-frame.plot_type = tecplot.constant.PlotType.XYLine
-plot = frame.plot()
-
-# clear plot
-plot.delete_linemaps()
-
-# create line plot from extracted zone data
-cp_linemap = plot.add_linemap(
-    name=extracted_slice.name,
-    zone=extracted_slice,
-    x=dataset.variable('x'),
-    y=dataset.variable('Pressure_Coefficient'))
-
-# integrate over the normalized extracted slice values
-scalar_var_index = dataset.variable('Pressure_Coefficient').index + 1
-command = '''
-    Integrate
-    VariableOption='Average'
-    XOrigin=0 YOrigin=0
-    ScalarVar={scalar_var}
-    XVariable=1
-'''.format(scalar_var=scalar_var_index)
-
-# print("Executing command:", command)  # Debug print to check command
-# tecplot.macro.execute_extended_command('CFDAnalyzer4', command)
-
-# get integral result from Frame's aux data
-total = float(frame.aux_data['CFDA.INTEGRATION_TOTAL'])
-
-# overlay result on plot in upper right corner
-frame.add_text('integration result: {:.5f}'.format(total), (60,90))
-
-# set style of linemap plot
-cp_linemap.line.color = tecplot.constant.Color.Blue
-cp_linemap.line.line_thickness = 0.8
-cp_linemap.y_axis.reverse = True
-
-# update axes limits to show data
-plot.view.fit()
-
-# export image of pressure coefficient as a function of x/c
-tecplot.export.save_png('wing_pressure_coefficient.png', 600, supersample=3)
+# save cl - r/R to txt tab separated
+np.savetxt("cl-r_R.txt", np.array([r_R_centers, cl_array]).T, delimiter="\t")
